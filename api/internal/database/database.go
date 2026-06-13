018be41529d6553d74cec26b8070b40f78a97bbe
rainbowbear <rainbow96bear@gmail.com>
2026-06-13 22:49:32 +0900

    supabase 연결 준비

--- api/internal/database/database.go
+++ api/internal/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"log"
 	"planet/internal/config"
 
 	"gorm.io/driver/postgres"
@@ -8,10 +9,23 @@ import (
 )
 
 func Connect(cfg *config.Config) (*gorm.DB, error) {
+	log.Printf("DB: %s", cfg.DB)
+
 	db, err := gorm.Open(postgres.Open(cfg.DB.DSN()), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("database connected")
 
 	return db, nil
 }
